12da4f87a0623ed9899a427fa267dc068584048b
alisson-moura <alisson.mo.moura@outlook.com.br>
2022-04-27 22:15:35 -0300

    fix: corrigido script

--- public/script.js
+++ public/script.js
@@ -4,21 +4,24 @@ const $file = document.getElementById('file')
 const $fileInput = document.getElementById('sendFile')
 const $formFile = document.getElementById('formFile')
 
-//set first file
-$active.setAttribute('src', $files.item(0).src)
-$file.value = $files.item(0).alt
+if ($files.length > 0) {
+  //set first file
+  $active.setAttribute('src', $files.item(0).src)
+  $file.value = $files.item(0).alt
 
-//send file
-$fileInput.addEventListener('change', e => {
-  $formFile.submit();
-})
-
-// add event on files
-for (let i = 0; i < $files.length; i++) {
-  $files.item(i).addEventListener('click', e => {
-    const src = e.target.attributes.src.value
-    $active.setAttribute('src', src)
-    $file.value = e.target.attributes.alt.value
+  //send file
+  $fileInput.addEventListener('change', e => {
+    $formFile.submit();
   })
+
+  // add event on files
+  for (let i = 0; i < $files.length; i++) {
+    $files.item(i).addEventListener('click', e => {
+      const src = e.target.attributes.src.value
+      $active.setAttribute('src', src)
+      $file.value = e.target.attributes.alt.value
+    })
+  }
+
 }
 
